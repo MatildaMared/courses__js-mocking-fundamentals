aaf1d42e6eeb8a3e502c6072a11825761eaf6e49
Matilda Mared <matildamared@live.se>
2023-01-11 15:52:50 +0100

    add a spyOn function

--- src/no-framework/spy.js
+++ src/no-framework/spy.js
@@ -4,31 +4,39 @@
  * Execute: Use `npx jest --watch src/no-framework/spy.js` to watch the test
  */
 
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
 function fn(impl = () => {}) {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args)
-    return impl(...args)
-  }
-  mockFn.mock = {calls: []}
-  return mockFn
+	const mockFn = (...args) => {
+		mockFn.mock.calls.push(args);
+		return impl(...args);
+	};
+	mockFn.mock = { calls: [] };
+	mockFn.mockImplementation = (newImpl) => (impl = newImpl);
+	return mockFn;
 }
 
-spyOn(utils, 'getWinner')
-utils.getWinner.mockImplementation((p1, p2) => p1)
+function spyOn(obj, name) {
+	const originalValue = obj[name];
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+	obj[name] = fn();
+	obj[name].mockRestore = () => (obj[name] = originalValue);
+}
+
+spyOn(utils, "getWinner");
+utils.getWinner.mockImplementation((p1, p2) => p1);
+
+const winner = thumbWar("Kent C. Dodds", "Ken Wheeler");
+assert.strictEqual(winner, "Kent C. Dodds");
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
-  ['Kent C. Dodds', 'Ken Wheeler'],
-  ['Kent C. Dodds', 'Ken Wheeler'],
-])
+	["Kent C. Dodds", "Ken Wheeler"],
+	["Kent C. Dodds", "Ken Wheeler"],
+]);
 
 // cleanup
-utils.getWinner.mockRestore()
+utils.getWinner.mockRestore();
 
 /**
  * Checkout master branch to see the answer.
